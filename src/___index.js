8546c70167562ace74d864848dfa19065e60556d
Mimieam <t.echismimi@gmail.com>
2018-01-09 01:33:56 -0500

    dereferencing index.html and redirecting it to popup.html

---
+++ src/___index.js
@@ -0,0 +1,8 @@
+/**
+ * The current index file is simply left as an old remenant from the CRA Eject 
+ * (script/start.js checks for it)
+ * 
+ * In a chrome app, you have 2 index.js
+ * the FrontEnd  in src/popup/index.js
+ * and the BackEnd in src/backgound/index.js
+ */
