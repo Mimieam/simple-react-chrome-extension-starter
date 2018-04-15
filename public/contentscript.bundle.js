702f52f2f37bf9163a3501c63c994e2402a00d7e
Mimieam <t.echismimi@gmail.com>
2018-04-15 08:23:22 -0500

    restructuring

--- public/contentscript.bundle.js
+++ public/contentscript.bundle.js
@@ -61,7 +61,7 @@
 /******/ 	
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "dc88319261fcfaccfcf6"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "168f3dd19e8afcd29019"; // eslint-disable-line no-unused-vars
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule; // eslint-disable-line no-unused-vars
